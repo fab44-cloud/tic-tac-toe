064f817d58e1a0f4e9df9a43fcc69ba366582706
Ford Berry <fordaberry@gmail.com>
2025-08-17 15:04:33 -0600

    Create methods that mark squares, reset the board, and get access to the board

--- script.js
+++ script.js
@@ -2,6 +2,23 @@
 const Gameboard = (() => {
     // gameboard array
     let boardArray = ["", "", "", "", "", "", "", "", ""];
+
+    // Methods
+    const markSquare = (index, playerSymbol) => {
+        // console.log(markSquare);
+        if (boardArray[index] === "") {
+            boardArray[index] === playerSymbol;
+        }
+        return false; // Indicates square was already marked
+    };
+
+    const reset = () => {
+        boardArray = ["", "", "", "", "", "", "", "", ""];
+    }
+
+    const getBoard = () => boardArray; // Allows access to the board without directly modifying it
+
+    return { markSquare, reset, getBoard };
 })();
 
 
