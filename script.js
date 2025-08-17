9fda98d8a92d2c032cc8e5277657710bbc74e75a
Ford Berry <fordaberry@gmail.com>
2025-08-17 16:29:06 -0600

    Set properties for control of the game

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const Gameboard = (() => {
 
     const getBoard = () => boardArray; // Allows access to the board without directly modifying it
 
-    return { markSquare, reset, getBoard };
+    return { markSquare, reset, getBoard }; // Publicly accessible methods of the Gameboard object.
 })();
 
 
@@ -29,3 +29,18 @@ const playerFactory = (name, symbol) => {
 
 const player1 = playerFactory("Player 1", 'X');
 const player2 = playerFactory("Player2", 'O');
+
+// Game flow control 
+const GameController = (() => {
+    // Properties
+    const players = [playerFactory("Player1", "X"), playerFactory("Player 2", "O")]
+    let currentPlayer = player1;
+    let gameActive = false;
+    let movesMade = 0;
+
+    const winningConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
+})();
